0457bd5a3972cb76775e99ff9db2b8ce3a498758
festus-sulumeti <sulumetifestus@outlook.com>
2023-10-20 15:32:09 +0300

    THe commits

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -19,13 +19,36 @@ const Hero = () => (
            text-white ss:leading-[100px] leading-[75px] 
           ">Rebs <br className="sm:block hidden" /> {" "}
           <span className="text-gradient">Microsystem </span> {" "}
-           Limited</h1>
+           Ltd</h1>
 
            <div className="ss:flex hidden md:mr-4 mr-0">
              <GetStarted />
            </div>
-       </div>        
+       </div> 
+
+       <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px]
+           text-white ss:leading-[100px] leading-[75px]  w-full
+       ">
+          Internet Access
+       </h1>    
+
+       <p className={` ${styles.paragraph} max-w-[470px] mt-5`}>We are a global internet providing company based in kakamega Kenya.
+        As a fast paced company, we believe in the quality internet services to our clients.
+       </p>   
       </div>
+
+      <div className={`flex-1 fles ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
+
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+      </div> 
+
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />  
+      </div>     
     </section>
 )
 
